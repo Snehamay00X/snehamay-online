5687fb8072b432de2497cb9d3e18401ca58ae535
Snehamay00X <snehamayhembram007@gmail.com>
2026-02-07 07:10:41 +0530

    fix: Reorder Next.js rewrite rules for `/projects/truechat` to correctly handle root and path matches.

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/projects/truechat",
-        destination: "https://frontend-puce-omega-74.vercel.app",
-      },
-      {
         source: "/projects/truechat/:path*",
         destination: "https://frontend-puce-omega-74.vercel.app/:path*",
+      },
+      {
+        source: "/projects/truechat",
+        destination: "https://frontend-puce-omega-74.vercel.app",
       },
     ];
   },
